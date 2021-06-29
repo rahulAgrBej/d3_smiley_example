355bea5a9c0fbb8619fbac653dd1ca1b8265dff6
rahulAgrBej <rahul.agrawal.b@gmail.com>
2021-06-28 20:56:18 -0400

    initial commit for smiley face example

--- src/index.js
+++ src/index.js
@@ -1,8 +1,44 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+
+const width = 960;
+const height = 500;
+const centerX = width / 2;
+const centerY = height / 2;
+const strokeWidth = 10;
+const eyeOffsetX = 90;
+const eyeOffsetY = 100;
+const eyeRadius = 50;
+
+const App = () => (
+  <svg width={width} height={height}>
+    <circle
+            cx= {centerX}
+            cy= {centerY}
+            r={centerY - (strokeWidth / 2)}
+            fill='yellow'
+            stroke='black'
+            strokeWidth={strokeWidth}/>
+    <circle
+            cx= {centerX - eyeOffsetX}
+            cy= {centerY - eyeOffsetY}
+            r={eyeRadius}/>
+    <circle
+            cx= {centerX + eyeOffsetX}
+            cy= {centerY - eyeOffsetY}
+            r={eyeRadius}/>
+  </svg>
+);
+
+const rootElement = document.getElementById('root');
+ReactDOM.render(<App />, rootElement);
+
+
+
+/*import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+
 
 ReactDOM.render(
   <React.StrictMode>
@@ -10,8 +46,9 @@ ReactDOM.render(
   </React.StrictMode>,
   document.getElementById('root')
 );
+*/
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+//reportWebVitals();
